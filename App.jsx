ca15e8f42acdf03c4b37b8830a855a62325af8fc
Rokesh_203 <rokeshkrishnan203@gmail.com>
2025-03-15 16:27:28 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,31 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const StateandEffect = () => {
+
+  const [score,setScore]=useState(0);
+  const [balls,setBalls]=useState(0);
+  const [overs,setOvers]=useState(0);
+
+  useEffect(()=>{
+    if(balls >= 6){
+      setOvers(overs+1);
+      setBalls(0);
+    }
+  },[balls]);
+
+  const addscore=(runs)=>{
+    setScore(score+runs);
+    setBalls(balls+1);
+  }
+;
   return (
     <div className='container' ><h2>Cricket ScoreBoard</h2>
     
     <div>
-        <h3>Score   :</h3>
-        <h3>Wickets :</h3>
-        <h3>Balls   :</h3>
-        <h3>Overs   :</h3>
+        <h3>Score   :{score}</h3>
+        <h3>Wickets : {wickets}</h3>
+        <h3>Balls   : {balls}</h3>
+        <h3>Overs   :{overs}</h3>
        
     </div>
 
     <div style={{display:"flex",gap:"10px"}}>
-        <button>1 RUN</button>
-        <button>2 RUN </button>
-        <button>3 RUN</button>
-        <button>4 RUN</button>
-        <button>5 RUN</button>
+        <button onClick={()=>addscore(1)}>1 RUN</button>
+        <button onClick={()=>addscore(2)}>2 RUN </button>
+        <button onClick={()=>addscore(3)}>3 RUN</button>
+        <button onClick={()=>addscore(4)}>4 RUN</button>
+        <button onClick={()=>addscore(6)}>6 RUN</button>
         
 
     </div>
 
-    <div style={{marginTop:"20px"}}>
-    <button style={{background:"read"}}>Wickets</button>
-
-    </div>
 
     </div>
   )
